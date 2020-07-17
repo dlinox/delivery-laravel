981f1e5aef7d896e694b7dbdb8c82957f2caa02d
denis-web <nearlino20@gmail.com>
2020-07-16 23:55:11 -0500

    Modulo de Adminsitardor-titulo

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                ADMINISTRADOR
                 </div>
 
                 <div class="links">
